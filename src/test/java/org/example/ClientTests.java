8bff51c991cb82c9e1d00560be8f20f8334f93fb
Traczyk <grzegorz.traczyk@capgemini.com>
2025-02-24 17:00:29 +0100

    [codeai] Generate unit_tests classes

---
+++ src/test/java/org/example/ClientTests.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.AfterAll;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ClientTests {
+
+    private Service serviceMock;
+    private Client client;
+
+    @BeforeEach
+    void setUp() {
+        serviceMock = Mockito.mock(Service.class);
+        client = new Client(serviceMock);
+    }
+
+   
+}
