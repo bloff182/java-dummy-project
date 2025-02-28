ec7d3621bc8e097e78d6f052e33aef0f6b3cc76f
Traczyk <grzegorz.traczyk@capgemini.com>
2025-02-28 14:13:22 +0100

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/org/example/ClientTests.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeAll;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.mock;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class ClientTests {
+
+    private Service serviceMock;
+    private Client client;
+
+    @BeforeEach
+    void setUp() {
+        serviceMock = Mockito.mock(Service.class);
+        client = new Client(serviceMock);
+    }
+
+    @Test
+    void greeting_ValidName_ReturnsGreeting() {
+        // GIVEN
+        Mockito.when(serviceMock.isEven(5)).thenReturn(false);
+        // WHEN
+        String result = client.greeting("John");
+        // THEN
+        assertThat(result).isEqualTo("Hello, John!"); 
+    }
+
+
+}
